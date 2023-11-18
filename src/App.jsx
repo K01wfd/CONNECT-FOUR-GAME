09a6c0a0aad669549ade42d13ce481e65bb7c7ef
Daniel Denkha <danialdnkha@gmail.com>
2023-11-19 01:35:55 +1100

    adding and styling game rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import MainMenu from './components/MainMenu';
+import GameRues from './components/GameRues';
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
-  let gameOptions = {};
-  return <>{!gameStarted && <MainMenu />}</>;
+  let gameOptions = { playing: false, rules: false };
+  return (
+    <>
+      {!gameStarted && <MainMenu />}
+      {gameOptions.rules && <GameRues />}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/GameRues.jsx
@@ -0,0 +1,51 @@
+import styles from '../styles/gameRules.module.css';
+
+function GameRues() {
+  return (
+    <article className={`${styles.gameRules} flow`} aria-label='game rules'>
+      <h1 className='fs-h1'>Rules</h1>
+      <div className='flow-medium'>
+        <div className='flow-small'>
+          <h2 className='fs-h3 text-purple'>OBJECTIVE</h2>
+          <p className='opacity-66'>
+            Be the first player to connect 4 of the same colored discs in a row
+            (either vertically, horizontally, or diagonally).
+          </p>
+        </div>
+        {/*  */}
+        <div className='flow-small'>
+          <h2 className='fs-h3 text-purple'>HOW TO PLAY</h2>
+          <ol className={`${styles.rulesList} flow-small`}>
+            <li className='flex'>
+              <span className='mr-s'>1</span>
+              <span className='opacity-66'>
+                Red goes first in the first game.
+              </span>
+            </li>
+            <li className='flex'>
+              <span className='mr-s'>2</span>
+              <span className='opacity-66'>
+                Players must alternate turns, and only one disc can be dropped
+                in each turn.
+              </span>
+            </li>
+            <li className='flex'>
+              <span className='mr-s'>3</span>
+              <span className='opacity-66'>
+                The game ends when there is a 4-in-a-row or a stalemate.
+              </span>
+            </li>
+            <li className='flex'>
+              <span className='mr-s'>3</span>
+              <span className='opacity-66'>
+                The starter of the previous game goes second on the next game.
+              </span>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default GameRues;
